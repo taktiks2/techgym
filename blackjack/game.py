18c802e7d723e0d1e8e2f139b268985ecb89d71e
taktiks2 <deathproof.lee@gmail.com>
2021-11-28 13:36:47 +0900

    edited game.py

--- blackjack/game.py
+++ blackjack/game.py
@@ -3,8 +3,19 @@ from card import *
 
 
 class Game:
+    PLAYERS: dict = {'player': 0,
+                     'dealer': 1}
+    SOFT_HAND: int = 0
+    HARD_HAND: int = 1
+    BLACKJACK: int = 21
+    
     deck: Deck = None
-    player: Player = None
+    players: list = []
+    bet_coin: int = 0
+    
+    turn: int = 0
+    burst_flag: bool = False
+    blackjack_flag: bool = False
 
 
 def b():
@@ -17,31 +28,61 @@ def show_start_msg():
 
 def set_game():
     Game.deck = Deck()
-    Game.player = Player()
+    for name in Game.PLAYERS:
+        Game.players.append(Player(name))
     
 
-def bet(self, coin: int):
-    self.coin -= coin
-    return coin
+def bet():
+    string = input('How much coins would you BET?(1-500): ')
+    Game.bet_coin = int(string)
+    Game.player.coin -= Game.bet_coin
+    
+
+def hit():
+    Game.player.hand.append(Deck.deal())
 
 
-def hit(self):
-    self.hand.append(Deck.deal())
+def stand():
+    Game.turn ^= 1
 
 
-def stand(self):
+def surrender():
     pass
 
 
-def surrender(self):
+def show_hand():
+    print()    
+
+def set_hand_value():
+    values: list = [0, 0]
+    for card in Game.player[Game.turn].hand:
+        if card.number == Number.ACE:
+            values[Game.SOFT_HAND] += 1
+            values[Game.HARD_HAND] += 11
+        elif card.number.value > 10:
+            values[Game.SOFT_HAND] += 10
+            values[Game.HARD_HAND] += 10
+        else:
+            values[Game.SOFT_HAND] += card.number.value
+            values[Game.HARD_HAND] += card.number.value
+    return values
+
+
+def judge_hand_vlaue():
+    values: list = set_hand_value()
+    for value in values:
+        if len(Game.player[Game.turn].hand) == 2 and value == Game.BLACKJACK:
+            Game.blackjack_flag = True
+        
+    
+     
+
+
+def burst():
     pass
 
 
-def burst(self):
-    pass
-
-
-def blackjack(self):
+def blackjack():
     pass
 
 
@@ -60,7 +101,10 @@ def show_card():
 
 
 def main():
-    pass
+    set_game()
+    bet()
+    
+    
 
 
 if __name__ == '__main__':
